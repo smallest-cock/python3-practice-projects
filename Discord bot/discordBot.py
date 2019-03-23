2e8b440de20b9b12b6f311230f73d964f4d9ffe0
smallest-cock <soccerstarslegend420@gmail.com>
2019-03-23 12:54:46 -0700

    SSL cert fix

--- Discord bot/discordBot.py
+++ Discord bot/discordBot.py
@@ -9,6 +9,11 @@ import discord
 from random import randint
 import asyncio
 import time
+import os
+import ssl
+if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
+        getattr(ssl, '_create_unverified_context', None)):
+    ssl._create_default_https_context = ssl._create_unverified_context
 
 client = discord.Client()
 
