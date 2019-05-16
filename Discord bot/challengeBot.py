b13021bc410091d4b554061c95d3995b7feb78f9
smallest-cock <soccerstarslegend420@gmail.com>
2019-05-16 01:12:50 -0700

    Updated ChallengeBot

--- Discord bot/challengeBot.py
+++ Discord bot/challengeBot.py
@@ -6,11 +6,11 @@ import asyncio
 
 client = discord.Client()
 
-channelInfo = (('BeginnerChallenges.txt', 555767513930268683),
-               ('NoviceChallenges.txt', 576870446545371161),
-               ('IntermediateChallenges.txt', 576870475955699758),
-               ('AdvancedChallenges.txt', 576870519974920202),
-               ('ExpertChallenges.txt', 576870496449069066))
+channelInfo = (('Beginner_Challenges.txt', 555767513930268683),
+               ('Novice_Challenges.txt', 576870446545371161),
+               ('Intermediate_Challenges.txt', 576870475955699758),
+               ('Advanced_Challenges.txt', 576870519974920202),
+               ('Expert_Challenges.txt', 576870496449069066))
 
 postedList = []
 
@@ -24,7 +24,7 @@ async def post_challenge_background_task():
                 with open(channel[0]) as f:  # opens text file that contains challenges
                     # channel ID number in parentheses
                     challengeChannel = client.get_channel(channel[1])
-                    modActionsChannel = client.get_channel(577326085289803776)
+                    modActionsChannel = client.get_channel(553343591834189835)
                     text = f.read()
                     # challenges must be separated by a blank line in the txt file
                     challengeList = text.split('\n\n')
